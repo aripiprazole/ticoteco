674950207660aa5a01ee0bcacc6513ae65b6d46b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-05 14:34:20 -0300

    style: add linters(lint-staged, eslint and commitlint)

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    mocha: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'google',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': ['off'],
+    'require-jsdoc': ['off'],
+    'spaced-comment': ['off'],
+  },
+};
