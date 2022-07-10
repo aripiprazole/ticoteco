ed29b0fdbb12bcacfdc45e7b99cdddc8be40f44e
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-10 04:20:35 -0300

    feat(ui): setup chromatic

---
+++ packages/ui/src/navigation/Navigation.stories.tsx
@@ -0,0 +1,32 @@
+/*
+ * TikTok clone as web application
+ * Copyright (C) 2022  Gabrielle Guimarães de Oliveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+
+import {Navigation} from './Navigation';
+
+export default {
+  title: 'UI Kit/Navigation',
+  component: Navigation,
+} as ComponentMeta<typeof Navigation>;
+
+export const HeaderNavigation: ComponentStory<typeof Navigation> = () => {
+  return <Navigation />;
+};
