30dacec5527e28ab6c224df7d2687d8399f8ad85
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-13 22:30:36 -0300

    chore: rename ~/ paths of tsconfig.json to @/

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {createServer} from '~/server';
+import {createServer} from '@/server';
 
 async function startBackend() {
   const app = createServer();
