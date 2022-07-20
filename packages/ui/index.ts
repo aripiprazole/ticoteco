0f6b85123fc11171fbe08e188b6c99e79d8737fb
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-19 23:33:16 -0300

    refactor(ui): move timeline to web package

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -22,7 +22,6 @@ export * from './src/authentication/AuthContext';
 // UI Components
 export * from './src/style/GlobalStyle';
 export * from './src/button/Button';
-export * from './src/timeline/Timeline';
 export * from './src/navigation/search/Search';
 export * from './src/navigation/auth-buttons/AuthButtons';
 export * from './src/navigation/Navigation';
