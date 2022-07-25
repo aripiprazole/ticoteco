d80cbecb325dc92f0a2ac0e50da9a2fbb687cfd6
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-25 00:39:15 -0300

    fix(web): remove styled-components from _document.tsx

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -18,49 +18,10 @@
 
 import React from 'react';
 
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
-
-import {ServerStyleSheet} from 'styled-components';
+import Document, {Head, Html, Main, NextScript} from 'next/document';
 
 /** App custom Next.js document */
 class AppDocument extends Document {
-  /**
-   * Get the initial props of the document with styled-components
-   *
-   * @param {DocumentContext} ctx
-   */
-  public static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
   public render(): JSX.Element {
     return (
       <Html>
