a16425c82496d288416049817255c972c78579c1
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-26 01:34:26 -0300

    feat(web): add video input

--- packages/web/src/components/navigation/Upload.tsx
+++ packages/web/src/components/navigation/Upload.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
+import React, {useRef} from 'react';
 
 import {FiUpload} from 'react-icons/fi';
 
@@ -36,6 +36,9 @@ import {
   ModalHeader,
   ModalOverlay,
   VStack,
+  HStack,
+  Flex,
+  Heading,
   useDisclosure,
 } from '@chakra-ui/react';
 
@@ -46,6 +49,7 @@ import * as Yup from 'yup';
 type UploadForm = {
   readonly title: string;
   readonly description: string;
+  readonly video: File | null;
 }
 
 function Upload() {
@@ -55,10 +59,12 @@ function Upload() {
     validationSchema: Yup.object({
       title: Yup.string().required().min(4).max(32),
       description: Yup.string().min(0).max(32),
+      video: Yup.mixed().required(),
     }),
     initialValues: {
       title: '',
       description: '',
+      video: null,
     },
     onSubmit: (values) => {
       console.log('success', values);
@@ -76,49 +82,17 @@ function Upload() {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
 
-        <ModalContent>
+        <ModalContent maxHeight='40rem' maxWidth='50rem'>
           <chakra.form>
             <ModalHeader>Upload a video</ModalHeader>
             <ModalCloseButton />
 
             <ModalBody>
-              <VStack gap='0.5rem'>
-                <FormControl isInvalid={Boolean(formik.errors.title)}>
-                  <FormLabel>Title</FormLabel>
+              <HStack gap='1rem' height='20rem' align='start'>
+                <VideoInputs formik={formik} />
 
-                  <Input
-                    name='title'
-                    title='Title'
-                    placeholder='Title'
-                    value={formik.values.title}
-                    onChange={formik.handleChange}
-                  />
-
-                  {formik.errors.title && (
-                    <FormErrorMessage>
-                      {formik.errors.title}
-                    </FormErrorMessage>
-                  )}
-                </FormControl>
-
-                <FormControl isInvalid={Boolean(formik.errors.description)}>
-                  <FormLabel>Description</FormLabel>
-
-                  <Input
-                    name='description'
-                    title='Description'
-                    placeholder='Description'
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                  />
-
-                  {formik.errors.description && (
-                    <FormErrorMessage>
-                      {formik.errors.description}
-                    </FormErrorMessage>
-                  )}
-                </FormControl>
-              </VStack>
+                <DataInputs formik={formik} />
+              </HStack>
             </ModalBody>
 
             <ModalFooter>
@@ -128,6 +102,93 @@ function Upload() {
         </ModalContent>
       </Modal>
     </>
+  );
+}
+
+type DataInputsProps = {
+  readonly formik: ReturnType<typeof useFormik<UploadForm>>;
+};
+
+function VideoInputs(props: DataInputsProps) {
+  const {formik} = props;
+
+  const inputRef = useRef<HTMLInputElement>();
+
+  return (
+    <Flex
+      padding='0 1.5rem'
+      height='100%'
+      border='2px dashed #cecece'
+      borderRadius='1rem'
+      align='center'
+      justify='center'
+    >
+      <VStack>
+        <Heading as='h3' fontSize='1.2rem'>
+          Select video to upload
+        </Heading>
+
+        <chakra.span fontSize='0.85rem' color='#5c5c5c'>
+          Only MP4
+        </chakra.span>
+
+        <Button onClick={() => inputRef.current.click()}>
+          Select file
+        </Button>
+
+        <Input
+          hidden
+          ref={inputRef}
+          type='file'
+          name='video'
+          onChange={formik.handleChange}
+        />
+      </VStack>
+    </Flex>
+  );
+}
+
+function DataInputs(props: DataInputsProps) {
+  const {formik} = props;
+
+  return (
+    <VStack gap='0.5rem'>
+      <FormControl isInvalid={Boolean(formik.errors.title)}>
+        <FormLabel>Title</FormLabel>
+
+        <Input
+          name='title'
+          title='Title'
+          placeholder='Title'
+          value={formik.values.title}
+          onChange={formik.handleChange}
+        />
+
+        {formik.errors.title && (
+          <FormErrorMessage>
+            {formik.errors.title}
+          </FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl isInvalid={Boolean(formik.errors.description)}>
+        <FormLabel>Description</FormLabel>
+
+        <Input
+          name='description'
+          title='Description'
+          placeholder='Description'
+          value={formik.values.description}
+          onChange={formik.handleChange}
+        />
+
+        {formik.errors.description && (
+          <FormErrorMessage>
+            {formik.errors.description}
+          </FormErrorMessage>
+        )}
+      </FormControl>
+    </VStack>
   );
 }
 
