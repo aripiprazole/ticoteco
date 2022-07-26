59a98ca068d17d9f5ae9ec238db979a088befb3f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-26 06:00:32 -0300

    build(backend): use babel to generate distribution

--- packages/backend/babel.config.js
+++ packages/backend/babel.config.js
@@ -1,15 +1,21 @@
+const {
+  createResolvePath,
+} = require('babel-plugin-tsconfig-paths-module-resolver');
+
+const defaultResolvePath = createResolvePath();
+
+function customResolvePath(sourceFile, currentFile, opts) {
+  const result = defaultResolvePath(sourceFile, currentFile, opts);
+
+  return result ? (result + '.js') : result;
+}
+
 module.exports = {
   presets: [
     '@babel/preset-typescript',
     ['@babel/preset-env', {targets: {node: 'current'}}],
   ],
-  plugins: [['babel-plugin-tsconfig-paths', {
-    relative: true,
-    extensions: [
-      '.ts',
-      '.js',
-    ],
-    rootDir: '.',
-    tsconfig: 'tsconfig.json',
-  }]],
+  plugins: [
+    ['tsconfig-paths-module-resolver', {resolvePath: customResolvePath}],
+  ],
 };
