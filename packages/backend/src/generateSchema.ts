df7b699b47ea19fe72304f7c4014ceba65e56f47
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-27 00:10:05 -0300

    refactor(backend): move generateSchema script to src

--- packages/backend/src/generateSchema.ts
+++ packages/backend/src/generateSchema.ts
@@ -20,7 +20,7 @@ import {printSchema} from 'graphql/utilities';
 import fs from 'fs/promises';
 import path from 'path';
 
-import schema from '../src/graphql/schema';
+import schema from './graphql/schema';
 
 async function generateSchema() {
   const targetFile = path.resolve(
