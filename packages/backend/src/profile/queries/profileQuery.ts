7c476e6327b5706697879b965e323b426b8ca1e3
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-07-30 06:29:25 -0300

    feat(backend): add base profile query structure

---
+++ packages/backend/src/profile/queries/profileQuery.ts
@@ -0,0 +1,47 @@
+/*
+ * TikTok clone as web application
+ * Copyright (C) 2022  Gabrielle Guimarães de Oliveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {GraphQLFieldConfig} from 'graphql/type';
+import {GraphQLNonNull, GraphQLString} from 'graphql';
+
+import TicoTecoContext from '../../graphql/TicoTecoContext.js';
+
+import GraphQLProfile from '../types/GraphQLProfile.js';
+import Profile from '../Profile.js';
+
+type ProfileArguments = {
+  readonly username: string;
+};
+
+type ProfileQuery = GraphQLFieldConfig<any, TicoTecoContext, ProfileArguments>;
+
+export const profileQuery: ProfileQuery = {
+  type: GraphQLProfile,
+  args: {
+    username: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The username of the profile',
+    },
+  },
+  description: 'Get the profile of the queried user',
+  resolve: async (_root, args) => {
+    const {username} = args;
+
+    return Profile.find({username});
+  },
+};
---
+++ packages/web/src/components/profile/Profile.tsx
@@ -0,0 +1,87 @@
+/*
+ * TikTok clone as web application
+ * Copyright (C) 2022  Gabrielle Guimarães de Oliveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+
+import graphql from 'babel-plugin-relay/macro';
+
+import {Box, Image, VStack} from '@chakra-ui/react';
+import {useLazyLoadQuery} from 'react-relay';
+
+const ProfileQuery = graphql`
+  query ProfileQuery($username: String!) {
+    profile(username: $username) {
+      id
+      username
+      displayName
+    }
+  }
+`;
+
+const ProfileVideos = graphql`
+  query ProfileVideosQuery($username: String!, $first: Int!, $after: String) {
+    profile(username: $username) {
+      videos(first: $first, after: $after) {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          startCursor
+          endCursor
+        }
+        edges {
+          cursor
+          node {
+            id
+            title
+            description
+            preview
+            video
+          }
+        }
+      }
+    }
+  }
+`;
+
+export type ProfileProps = {
+  readonly username: string;
+}
+
+function Profile(props: ProfileProps) {
+  const query = useLazyLoadQuery(ProfileQuery, {});
+
+  return <Content profile={{}} />;
+}
+
+type ContentProps = {
+  readonly profile: any;
+};
+
+function Content(props: ContentProps) {
+  const {profile} = props;
+
+  return (
+    <VStack>
+      <VStack>
+        <Image src={profile.avatar} />
+      </VStack>
+    </VStack>
+  );
+}
+
+export default Profile;
